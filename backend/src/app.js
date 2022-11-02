7e118405410a16060fb0562ddfa2dbbdb5114bd8
HarshitRaghav55 <harshit.raghav_cs19@gla.ac.in>
2022-11-02 17:44:36 +0530

    init

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("./database/connection")
-const Student = require("./models/students")
-const routerStudent = require("./routes/student");
+const Employee = require("./models/employes")
+const routerEmployee = require("./routes/employees");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(routerStudent);
+app.use(routerEmployee);
 
 app.listen(port, () => {
     console.log(`connection on port ${port}`);
